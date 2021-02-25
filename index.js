d6b0647f917e526d63e65c4654f50c5f2efdb6c9
Ian Cossentino <icossentino@gmail.com>
2021-02-25 10:37:16 -0500

    commit tree 8031033b55eebc81a57e5a089f5b1998e35cbc0e

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Add your code here
+
+
+function submitData(name, email) {
+  const url = 'http://localhost:3000/users'
+  const formData = {
+    name: name,
+    email: email
+  }
+  const configObj = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(formData)
+  }
+  return fetch(url, configObj)
+    .then(function(response) {
+      return response.json()
+    })
+    .then(function(object) {
+      appendObject(object)
+    })
+    .catch(function(error) {
+      const div = document.createElement('div')
+      div.innerHTML = error.message
+      document.body.appendChild(div)
+    })
+}
+
+
+function appendObject(object) {
+  const id = object.id
+  const div = document.createElement('div')
+  div.innerHTML = id
+  document.body.appendChild(div)
+  console.log(object)
+}
